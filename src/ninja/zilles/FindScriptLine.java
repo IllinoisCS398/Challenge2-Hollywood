d977381c0c931670cff92489f971e48a0a2f39a0
Craig Zilles <zilles@illinois.edu>
2016-06-21 17:11:30 -0500

    Initial commit

---
+++ src/ninja/zilles/FindScriptLine.java
@@ -0,0 +1,15 @@
+package ninja.zilles;
+
+/**
+ * A program to search for specific lines and print their line number.
+ * See FindScriptLine.txt for more details
+ * TODO: add your netid to the line below
+ * @author put-your-netid-here
+ */
+public class FindScriptLine {
+
+	public static void main(String[] args) {
+// TODO: Implement the functionality described in FindScriptLine.txt
+// TODO: Test your code manually and using the automated unit tests in FindScriptLineTest		
+	}
+}
